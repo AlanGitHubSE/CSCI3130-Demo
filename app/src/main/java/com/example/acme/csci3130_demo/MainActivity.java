9a0d2080ab5c845c18617d69101ac2aaaf43bb5e
AlanGitHubSE <zy537980@dal.ca>
2018-05-16 22:24:22 -0300

    Added new UI elements

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void StartCopy(View view){
+        EditText inputField = (EditText) findViewById(R.id.inputTextField);
+        TextView existField = (TextView) findViewById(R.id.existTextField);
+
+        existField.setText(inputField.getText().toString());
     }
 }
